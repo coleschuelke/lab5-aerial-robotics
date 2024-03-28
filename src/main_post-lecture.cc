9d9fa8b3e4da2e67a56427a5e586b1c2ae1896c4
Todd Humphreys <todd.humphreys@utexas.edu>
2024-03-28 09:40:20 -0500

    wip

--- src/main_post-lecture.cc
+++ src/main_post-lecture.cc
@@ -19,11 +19,11 @@ int main(int argc, char** argv) {
   z << 4, 10, 6, 5;
   Hr << 1, 0, 3, 0, 2, 6, 0, 0, 0, 0, 0, 1e-6;
   R = 3 * Eigen::MatrixXd::Identity(nz, nz);
-  Eigen::MatrixXd Rinv = R.inverse();
-  // This is the straightforward way to solve the the normal equations:
+  auto Rinv = R.inverse();
+  // This is the straightforward way to solve the the normal equations
   xHat = (Hr.transpose() * Rinv * Hr).inverse() * (Hr.transpose() * Rinv * z);
   std::cout << "The straightforward solution is \n" << xHat << std::endl;
-  // This method is similar but more numerically stable:
+  // This method is similar but more numerically stable
   xHat = (Hr.transpose() * Rinv * Hr).ldlt().solve(Hr.transpose() * Rinv * z);
   std::cout << "The ldlt-based solution is \n" << xHat << std::endl;
 
@@ -31,11 +31,12 @@ int main(int argc, char** argv) {
   StructureComputer structureComputer;
   // Create shared pointers to two CameraBundle objects.  The make_shared
   // function creates the objects and returns a shared pointer to each.
-  std::shared_ptr<CameraBundle> cb1 = std::make_shared<CameraBundle>();
-  std::shared_ptr<CameraBundle> cb2 = std::make_shared<CameraBundle>();
-  // Fill cb1's and cb2's data members with example contents.
+  auto cb1 = std::make_shared<CameraBundle>();
+  auto cb2 = std::make_shared<CameraBundle>();
+  // Fill cb1's and cb2's data members with dummy contents
   cb1->rx << 23, 56;
-  cb1->RCI.fill(3);
+  cb1->RCI = Eigen::MatrixXd::Identity(3, 3);
+  cb2->RCI = Eigen::MatrixXd::Identity(3, 3);
   cb1->rc_I(0) = 1;
   cb1->rc_I(1) = 0.4;
   // Clear out structureComputer
